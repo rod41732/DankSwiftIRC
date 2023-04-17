83b8b9773600c8e7339e58d2e5006cd8e0e41e35
road <rod8711@gmail.com>
2023-04-18 01:17:13 +0700

    always strip username prefix

--- Sources/DankSwiftIRC/MessageTypes/PrivMessage.swift
+++ Sources/DankSwiftIRC/MessageTypes/PrivMessage.swift
@@ -16,11 +16,11 @@ extension String {
     let u = unicodeScalars
     let fromIndex = u.index(u.startIndex, offsetBy: from)
     let toIndex = u.index(u.startIndex, offsetBy: to)
-    return String(u[fromIndex..<toIndex])
+    return String(u[fromIndex ..< toIndex])
   }
 }
 
-public func parseEmote(_ part: String, message: String) -> [PrivMessageEmote] {  // 100000:1-2,3-4
+public func parseEmote(_ part: String, message: String) -> [PrivMessageEmote] { // 100000:1-2,3-4
   let parts = part.components(separatedBy: ":")
 
   let emoteID = parts[0]
@@ -30,13 +30,13 @@ public func parseEmote(_ part: String, message: String) -> [PrivMessageEmote] { 
     return (Int(nums[0])!, Int(nums[1])!)
   }
   let (from, to) = positions[0]
-  let name = message.unicodeSubstring(from: from, to: to + 1)  // twitch indexing is inclusive end
+  let name = message.unicodeSubstring(from: from, to: to + 1) // twitch indexing is inclusive end
 
   return positions.map { it in PrivMessageEmote(emoteID: emoteID, name: name, position: it) }
 }
 
 public class PrivMessage: TwitchMessage {
-  public var userColor: String  // color in hex like #aabbcc, or empty if user haven't set any color
+  public var userColor: String // color in hex like #aabbcc, or empty if user haven't set any color
   public var userLogin: String
   public var displayName: String
   public var emotes: [PrivMessageEmote]
@@ -45,7 +45,7 @@ public class PrivMessage: TwitchMessage {
   public var channelID: String
 
   public var message: String
-  public var isAction: Bool  // is /me message
+  public var isAction: Bool // is /me message
 
   public var raw: IRCMessage
 
@@ -85,35 +85,24 @@ public class PrivMessage: TwitchMessage {
       parentDisplayName = irc.tag["reply-parent-display-name"]!
       parentBody = irc.tag["reply-parent-msg-body"]!
     }
-
     super.init(id: irc.tag["id"]!, timestamp: Int64(irc.tag["tmi-sent-ts"]!)!)
+
+    stripReplyUsernamePrefix()
   }
 
-  private func copy() -> PrivMessage {
-    return PrivMessage(irc: raw)
-  }
-
-  public func strippedUsernamePrefix() -> PrivMessage {
-    let copy = copy()
-    guard parentMessageId != nil else { return copy }
-    guard message.starts(with: "@" + parentUserLogin!) else { return copy }
+  private func stripReplyUsernamePrefix() {
+    guard parentMessageId != nil else { return }
+    guard message.starts(with: "@" + parentUserLogin!) else { return }
     let prefixLength = 2 + parentUserLogin!.unicodeScalars.count
     let strippedMessage = message.unicodeSubstring(from: prefixLength, to: message.unicodeScalars.count)
-    let offsetedEmotes = emotes.map {it in 
+    let offsetedEmotes = emotes.map { it in
       PrivMessageEmote(
-        emoteID: it.emoteID, 
-        name: it.name, 
+        emoteID: it.emoteID,
+        name: it.name,
         position: (it.position.0 - prefixLength, it.position.1 - prefixLength)
-      ) }
-
-    copy.message = strippedMessage
-    copy.emotes = offsetedEmotes
-
-    return copy
+      )
+    }
+    message = strippedMessage
+    emotes = offsetedEmotes
   }
-
-
-
-
-
 }
